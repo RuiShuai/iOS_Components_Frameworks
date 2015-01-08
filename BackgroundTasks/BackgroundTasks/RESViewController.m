45ab0a8aea708730625ee335d97b9e998c9de61a
taojj <sinotao1@gmail.com>
2015-01-08 21:25:52 +0800

    BackgroundTasks 项目准备

---
+++ BackgroundTasks/BackgroundTasks/RESViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  BackgroundTasks
+//
+//  Created by taotao on 15/1/8.
+//  Copyright (c) 2015年 RuiShuai Co., Ltd. All rights reserved.
+//
+
+#import "RESViewController.h"
+
+@interface RESViewController ()
+
+@end
+
+@implementation RESViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
