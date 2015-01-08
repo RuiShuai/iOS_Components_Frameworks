0b915958b1a3d47284b33b5392fee98b7ab548b2
taojj <sinotao1@gmail.com>
2015-01-08 21:28:38 +0800

    GesturePlayground 项目准备

---
+++ GesturePlayground/GesturePlayground/RESAppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GesturePlayground
+//
+//  Created by taotao on 15/1/8.
+//  Copyright (c) 2015年 RuiShuai Co., Ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RESAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GesturePlayground/GesturePlayground/RESViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GesturePlayground
+//
+//  Created by taotao on 15/1/8.
+//  Copyright (c) 2015年 RuiShuai Co., Ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RESViewController : UIViewController
+
+
+@end
+
