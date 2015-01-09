fd7633fca3fa1c3c86be7604be4def96d18b1146
taojj <sinotao1@gmail.com>
2015-01-09 18:28:59 +0800

    MyNotes2 项目准备

---
+++ MyNotes2/MyNotes2/RESViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MyNotes2
+//
+//  Created by taotao on 15/1/9.
+//  Copyright (c) 2015年 RuiShuai Co., Ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RESViewController : UIViewController
+
+
+@end
+
