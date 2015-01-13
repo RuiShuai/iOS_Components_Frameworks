ad623d5fc27c34e1b105e6788b113c91c6106835
taojj <sinotao1@gmail.com>
2015-01-13 21:02:17 +0800

    WhackACac项目准备

---
+++ WhackACac_GameCenter/WhackACac_GameCenter/RESAppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WhackACac_GameCenter
+//
+//  Created by taotao on 15/1/13.
+//  Copyright (c) 2015年 RuiShuai Co., Ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
